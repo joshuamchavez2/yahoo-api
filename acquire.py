72f97933e4ae000500995c670b4f7474beb96527
joshuamchavez2 <joshua.m.chavez2@gmail.com>
2022-01-09 15:38:59 -0600

    Docustrings and comments

--- acquire.py
+++ acquire.py
@@ -4,53 +4,101 @@ from env import api_key
 from datetime import timedelta, datetime
 
 def get_data():
-
+    '''
+    This functions brings all other functions together
+    First we grab all the top gainers for the day callign get_day_gainers
+    Checks to see how many total stocks there are 
+    API can get prices of 10 total per request so we bundle them up by 10
+    Request 10 at time and load them into the main dataframe df
+    return df
+    '''
+    # Set headers to include my api_key
     headers = {
         'x-api-key': api_key
         }
 
+    # Get day gainers
     list_of_symbols = get_day_gainers()
+
+    # Create an emtpy DataFrame
     df = pd.DataFrame()
+
+    # Check the size of the todays top gainers list
     total = len(list_of_symbols)
+
+    # Create a variable named end to be where the range will end
     end = ((total//10)*10)+10
+
+    # Check to see if total is evenly divisible by 10.  This is because api can grab 10 symbols per request
     if total%10 !=0:
+
+        # If the total is not evenly divisible by 10, loop through list by 10s, then grab the ones that are left later
         for y in range(10, end, 10):
             
+            # Setting x to be 10 less than y
             x = y-10
-
+            
+            # Creating a string called placeholder to be from range x:y (groups of 10) in the long list of symbols
             placeholder = create_placeholder(list_of_symbols[x:y])
+            
+            # Requesting all 10 stocks last 5 years of prices
             response = requests.request("GET", f'https://yfapi.net/v8/finance/spark?interval=1d&range=5y&symbols={placeholder}', headers=headers)
+            
+            # Save the response to data
             data = response.json()
             
+            # Creating an empty temporary Data Frame
             temp = pd.DataFrame()
-            # Using the function defined above to create a Data Frame
+
+            # Extracting the prices from json file
             temp = extract_date_and_price(data)
 
+            # Adding the 10 stocks last 5 years to the main DataFrame df
             df = pd.concat([df, temp], axis = 1)
         
+        # Clearing temp
         temp = pd.DataFrame()
+
+        # Grabbing the rest, should be less than 10 stocks symbols left
         placeholder = create_placeholder(list_of_symbols[end-10:total])
+
+        # Repeating the process to get the stock information for the last remaining symbols
         response = requests.request("GET", f'https://yfapi.net/v8/finance/spark?interval=1d&range=5y&symbols={placeholder}', headers=headers)
+        
+        # Saving response to data again
         data = response.json()
 
-        # Using the function defined above to create a Data Frame
+        # Extracting once more
         temp = extract_date_and_price(data)
 
+        # Adding the final symbols to the main dataframe df
         df = pd.concat([df, temp], axis = 1)
+
     else:
+
+        # If total is evenly divisible by 10, no need to add the extra step of getting the rest
+        # Just Loop by 10
         for y in range(10, end, 10):
-
+            
+            # Setting x to be 10 less than y
             x = y-10
-
+            
+            # Creating a string called placeholder to be from range x:y (groups of 10) in the long list of symbols
             placeholder = create_placeholder(list_of_symbols[x:y])
+            
+            # Requesting all 10 stocks last 5 years of prices
             response = requests.request("GET", f'https://yfapi.net/v8/finance/spark?interval=1d&range=5y&symbols={placeholder}', headers=headers)
+            
+            # Save the response to data
             data = response.json()
             
+            # Creating an empty temporary Data Frame
             temp = pd.DataFrame()
-            
-            # Using the function defined above to create a Data Frame
+
+            # Extracting the prices from json file
             temp = extract_date_and_price(data)
 
+            # Adding the 10 stocks last 5 years to the main DataFrame df
             df = pd.concat([df, temp], axis = 1)
 
     return df 
@@ -75,6 +123,7 @@ def create_placeholder(list_of_symbols):
         # All other index's will require a comma
         else:
             placeholder += symbol + ","
+
     return placeholder
 
 def get_day_gainers():
